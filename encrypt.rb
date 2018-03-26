4db8893179fe0d852900a5c0f0b81fdcf231d773
baur <bkozhaev@gmail.com>
2018-03-26 15:25:29 +0400

    SHA2

--- encrypt.rb
+++ encrypt.rb
@@ -27,6 +27,7 @@ string = STDIN.gets.chomp
 puts "Каким способом зашифровать:"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 encrypt_method = STDIN.gets.to_i
 
 until encrypt_method.between?(1, 2)
@@ -40,4 +41,5 @@ puts "Вот что получилось:"
 case encrypt_method
 when 1 then puts Digest::MD5.hexdigest(string)
 when 2 then puts Digest::SHA1.hexdigest(string)
+when 3 then puts Digest::SHA2.hexdigest(string)
 end
